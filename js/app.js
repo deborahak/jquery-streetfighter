08186f4bf85bb65296abfb6b82dfa71c5b73035e
Deborah Kain <deborahkain@mac.com>
2016-08-24 10:57:42 -0700

    keyup added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+$(document).keydown(function(event){
+  if (event.which == 88){
+    $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-cool').show();
+  };
+$(document).keyup(function(event){
+  if (event.which == 88) {
+    $('.ryu-cool').hide();
+    $('.ryu-ready').show();
+    $('.ryu-throwing').hide();
+  };
+
 $(document).ready(function(){
+  // "waits for the entire page to load"
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
@@ -26,16 +40,12 @@ $(document).ready(function(){
     $('.ryu-ready').show();
     //ryu goes to ready
   });
-});
+
 function playHadouken(){
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 };
-
-
-
-
 
 
 
